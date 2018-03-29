1eea59d8b028920c8516c35e74bac79a9b9643d2
ahmedoashery <ahmedoashery@gmail.com>
2018-03-29 17:18:40 +0200

    Set The Solution Structure And, added shell ,modules and libraries

---
+++ Data/Core/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+using Queries.Core.Repositories;
+using System;
+
+namespace EFTuts.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICourseRepository Courses { get; }
+        IAuthorRepository Authors { get; }
+        int Complete();
+    }
+}
---
+++ Data/UnitOfWork/Context.cs
@@ -0,0 +1,24 @@
+using Queries.Core.Domain;
+using Queries.Persistence.EntityConfigurations;
+using System.Data.Entity;
+
+namespace EFTuts.UnitOfWork
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base("name=PlutoContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CourseConfiguration());
+        }
+    }
+}
